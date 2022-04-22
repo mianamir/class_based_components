74378d430581a02f5b8607018645ca231dae80d0
Muhammad Amir <mianamirlahore@gmail.com>
2022-04-22 23:45:14 +0500

    user finder functional component in progress
    user finder functional component in progress.

---
+++ src/components/UserFinder.js
@@ -0,0 +1,37 @@
+import { Fragment, useState, useEffect } from "react";
+
+import Users from './Users';
+import classes from './UserFinder.module.css';
+
+const UserFinder = () => {
+
+    const DUMMY_USERS = [
+        { id: 'u1', name: 'Max' },
+        { id: 'u2', name: 'Manuel' },
+        { id: 'u3', name: 'Julie' },
+];
+
+    const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        setFilteredUsers(
+            DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+        );
+    }, [searchTerm]);
+
+    const searchChangeHandler = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    return (
+        <Fragment>
+          <input className={classes.finder} type='search' onChange={searchChangeHandler} />
+          <Users users={filteredUsers} />
+        </Fragment>
+      );
+
+};
+
+export default UserFinder;
