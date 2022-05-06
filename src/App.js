77f144864578668d8aa1c501cadf5ed8b347b586
Muhammad Amir <mianamirlahore@gmail.com>
2022-05-06 15:41:00 +0500

    final code refactoring.
    final code refactoring.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import UserFinder from './components/UserFinder';
 import UsersContext from './store/users-context';
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+  { id: 'u1', name: 'Amir' },
+  { id: 'u2', name: 'Muhammad' },
+  { id: 'u3', name: 'Muhammad Amir' },
 ];
 
 function App() {
